4b216e0954457e940ee50c41db05e505d51c1b8b
Nithanim <git@nithanim.me>
2020-03-29 21:24:33 +0200

    Use proper maven multimodule system

--- formats/format-bmd/src/main/java/module-info.java
+++ formats/format-bmd/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module culturestools.format.bmd {
   requires static lombok;
-  requires static metainf.services;
   requires org.apache.commons.io;
   exports me.nithanim.cultures.format.bmd;
   requires java.desktop;
--- formats/format-lib/src/main/java/module-info.java
+++ formats/format-lib/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module culturestools.format.lib {
   requires static lombok;
-  requires static metainf.services;
   requires org.apache.commons.io;
   exports me.nithanim.cultures.format.lib;
   exports me.nithanim.cultures.format.lib.io;
