ac2458c1781e2abc4136025d39e7462e97a331b1
Nithanim <git@nithanim.me>
2020-04-06 15:30:22 +0200

    Added bmd frame selection and stats

--- gui/src/main/java/me/nithanim/cultures/multitool/BmdToolController.java
+++ gui/src/main/java/me/nithanim/cultures/multitool/BmdToolController.java
@@ -5,10 +5,13 @@ import java.io.DataInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.control.Tooltip;
 import javafx.scene.image.Image;
@@ -32,39 +35,108 @@ public class BmdToolController implements Initializable {
   @FXML private TextField tfPcxPath;
   @FXML private CheckBox chbPcxSync;
   @FXML private FlowPane paneFrames;
+  @FXML private Label lblFrameStats;
+  @FXML private CheckBox chbFrameSelection;
+  @FXML private TextField tfFrameSelection;
 
   private BmdFile bmdFile;
   private PcxFile pcxFile;
 
   @Override
-  public void initialize(URL location, ResourceBundle resources) {}
+  public void initialize(URL location, ResourceBundle resources) {
+    chbFrameSelection
+        .selectedProperty()
+        .addListener(
+            (observable, oldValue, newValue) -> {
+              tfFrameSelection.setDisable(!newValue);
+              updateFrames();
+            });
+    tfFrameSelection.textProperty().addListener(observable -> updateFrames());
+  }
 
   @SneakyThrows
   private void setBmd(TreeData treeData) {
     tfBmdPath.setText(treeData.getFullPath());
     bmdFile = readBmd(treeData.getData().getInputStream());
-    updateFrames();
+    onFileSelectionChanged();
   }
 
   @SneakyThrows
   private void setPcx(TreeData treeData) {
     tfPcxPath.setText(treeData.getFullPath());
     pcxFile = new PcxFileReader().read(treeData.getData().getInputStream());
+    onFileSelectionChanged();
+  }
+
+  private void onFileSelectionChanged() {
+    updateBmdStats();
     updateFrames();
   }
 
-  private void updateFrames() throws BmdDecodeException {
+  private void updateBmdStats() {
+    if (bmdFile == null) {
+      lblFrameStats.setText("");
+    } else {
+      StringBuilder sb = new StringBuilder();
+      sb.append("Frames: ").append(bmdFile.getSize()).append('\n');
+      Map<Integer, Long> byFrameType =
+          bmdFile.getRawBmdFile().getFrameInfo().stream()
+              .map(BmdFrameInfo::getType)
+              .collect(Collectors.groupingBy(i -> i, Collectors.counting()));
+      sb.append("Frames Type 1: ").append(byFrameType.getOrDefault(1, 0L)).append('\n');
+      sb.append("Frames Type 2: ").append(byFrameType.getOrDefault(2, 0L)).append('\n');
+      sb.append("Frames Type 4: ").append(byFrameType.getOrDefault(4, 0L)).append('\n');
+      sb.append("Frames empty: ")
+          .append(
+              bmdFile.getRawBmdFile().getFrameInfo().stream()
+                  .filter(i -> i.getWidth() == 0 || i.getLen() == 0)
+                  .count())
+          .append('\n');
+      lblFrameStats.setText(sb.toString());
+    }
+  }
+
+  @SneakyThrows
+  private void updateFrames() {
     if (bmdFile != null && pcxFile != null) {
       paneFrames.getChildren().clear();
-      for (int i = 0; i < bmdFile.getSize(); i++) {
-        BufferedImage img = bmdFile.getFrame(i, pcxFile.getPalette());
-        if (img != null) {
-          Image image = FxUtil.convertToFxImage(img);
-          ImageView imageView = new ImageView(image);
-          setTooltipCreator(i, imageView);
-          paneFrames.getChildren().add(imageView);
-        }
+      int start = getRenderingStartFrame();
+      int end = getRenderingEndFrame();
+      for (int i = start; i < end; i++) {
+        addFrameToView(i);
       }
+    }
+  }
+
+  private int getRenderingStartFrame() {
+    int userSelectedFrame = getUserSelectedFrame();
+    return Math.max(userSelectedFrame, 0);
+  }
+
+  private int getRenderingEndFrame() {
+    int userSelectedFrame = getUserSelectedFrame();
+    if (userSelectedFrame >= 0) {
+      return Math.min(userSelectedFrame + 1, bmdFile.getSize());
+    } else {
+      return bmdFile.getSize();
+    }
+  }
+
+  private int getUserSelectedFrame() {
+    try {
+      return Integer.parseInt(tfFrameSelection.getText());
+    } catch (NumberFormatException ex) {
+      return -1;
+    }
+  }
+
+  private void addFrameToView(int i) throws BmdDecodeException {
+    BufferedImage img = bmdFile.getFrame(i, pcxFile.getPalette());
+    if (img != null) {
+      Image image = FxUtil.convertToFxImage(img);
+      ImageView imageView = new ImageView(image);
+      setTooltipCreator(i, imageView);
+      paneFrames.getChildren().add(imageView);
     }
   }
 
