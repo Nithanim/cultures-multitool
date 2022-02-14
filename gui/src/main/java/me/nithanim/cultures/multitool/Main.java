06316b6a73bbd379f5d616218fecdba8631c9595
Nithanim <git@nithanim.me>
2022-02-14 21:44:59 +0100

    Build distributable

--- gui/src/main/java/me/nithanim/cultures/multitool/Main.java
+++ gui/src/main/java/me/nithanim/cultures/multitool/Main.java
@@ -1,37 +1,9 @@
 package me.nithanim.cultures.multitool;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.stage.Stage;
-import picocli.CommandLine;
 
-public class Main extends Application {
-
+public class Main {
   public static void main(String[] args) {
-    Application.launch(Main.class, args);
-  }
-
-  @Override
-  public void start(Stage stage) throws Exception {
-    Cli cli = new Cli();
-    new CommandLine(cli).parseArgs(getParameters().getRaw().toArray(String[]::new));
-
-    MainController controller = new MainController();
-
-    FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/main.fxml"));
-    loader.setController(controller);
-    loader.setClassLoader(this.getClass().getClassLoader());
-    Parent root = loader.load();
-    Scene scene = new Scene(root);
-    scene.getStylesheets().add(this.getClass().getResource("/styles/styles.css").toString());
-    stage.setTitle("Cultures Multitool");
-    stage.setScene(scene);
-    stage.show();
-
-    if (cli.fileToOpen != null) {
-      controller.openFile(cli.fileToOpen);
-    }
+    Application.launch(MainFx.class, args);
   }
 }
---
+++ gui/src/main/java/me/nithanim/cultures/multitool/MainFx.java
@@ -0,0 +1,32 @@
+package me.nithanim.cultures.multitool;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import picocli.CommandLine;
+
+public class MainFx extends Application {
+  @Override
+  public void start(Stage stage) throws Exception {
+    Cli cli = new Cli();
+    new CommandLine(cli).parseArgs(getParameters().getRaw().toArray(String[]::new));
+
+    MainController controller = new MainController();
+
+    FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/main.fxml"));
+    loader.setController(controller);
+    loader.setClassLoader(this.getClass().getClassLoader());
+    Parent root = loader.load();
+    Scene scene = new Scene(root);
+    scene.getStylesheets().add(this.getClass().getResource("/styles/styles.css").toString());
+    stage.setTitle("Cultures Multitool");
+    stage.setScene(scene);
+    stage.show();
+
+    if (cli.fileToOpen != null) {
+      controller.openFile(cli.fileToOpen);
+    }
+  }
+}
